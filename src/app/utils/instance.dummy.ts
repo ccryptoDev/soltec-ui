fa3ba4ea6afe7273183b045ce7e2a3fec3a6b6fd
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-02-26 12:01:38 -0400

    added dummy data for projects

---
+++ src/app/utils/instance.dummy.ts
@@ -0,0 +1,86 @@
+export const instanceDummy = {
+  "name": "Versión 1.0",
+  "description": "La primera versión",
+  "files": [
+    {
+      "id": "hs34j-4h9fhh-oidh49-4343",
+      "fileName": "example_layout1.dxf",
+      "fileSizeMb": 1.2,
+      "fileCategory": "layout",
+      "createdAt": "2023-10-27T11:20:15Z"
+    },
+    {
+      "id": "hs34j-4h9fhh-oidh49-4344",
+      "fileName": "zone.dxf",
+      "fileSizeMb": 1.2,
+      "fileCategory": "layout",
+      "createdAt": "2024-01-22T11:20:15Z"
+    },
+    {
+      "id": "hs34j-4h9fhh-oidh49-4345",
+      "fileName": "layout4.dxf",
+      "fileSizeMb": 1.2,
+      "fileCategory": "layout",
+      "createdAt": "2023-03-11T11:20:15Z"
+    },
+    {
+      "id": "hs34j-4h9fhh-oidh49-4346",
+      "fileName": "file8.dxf",
+      "fileSizeMb": 1.2,
+      "fileCategory": "layout",
+      "createdAt": "2023-11-19T11:20:15Z"
+    },
+    {
+      "id": "hs34j-4h9fhh-oidh49-4347",
+      "fileName": "apend8.dxf",
+      "fileSizeMb": 1.2,
+      "fileCategory": "layout",
+      "createdAt": "2024-02-02T11:20:15Z"
+    },
+    {
+      "id": "hs34j-4h9fhh-oidh49-4348",
+      "fileName": "direction.dxf",
+      "fileSizeMb": 1.2,
+      "fileCategory": "layout",
+      "createdAt": "2023-12-30T11:20:15Z"
+    },
+  ],
+  "coordinateSets": [
+    {
+      "colorGroup": "BT1",
+      "colorCode": "FF00FF",
+      "coordinates": [
+        {
+          "tracker_id": "tracker_01",
+          "x_north": 345.432,
+          "y_north": 123.456,
+          "x_south": 345.432,
+          "y_south": 103.456
+        }
+      ]
+    }
+  ],
+  "trackerWidth": 4.5,
+  "trackers": 192,
+  "bifila": 10,
+  "bracktrackingGroups": 2,
+  "history": [
+    {
+      "user": "Diego Pérez",
+      "changes": "Calculo de archivos",
+      "date": "2023-10-26T13:45:00+00:00"
+    }
+  ]
+};
+
+export const instanceDummy_1 = {
+  "name": "Versión 1.0",
+  "description": "La primera versión",
+  "trackerWidth": 4.5,
+  "trackers": 192,
+  "bifila": 10,
+  "bracktrackingGroups": 2,
+  "files": [],
+  "coordinateSets": [],
+  "history": []
+}
