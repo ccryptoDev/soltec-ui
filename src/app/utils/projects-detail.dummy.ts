ad2cb1d7d2e3561d9371dc5d80d7ca6c2b914db6
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-02-26 12:01:22 -0400

    added dummy data for project-detail

---
+++ src/app/utils/projects-detail.dummy.ts
@@ -0,0 +1,147 @@
+export const projectsDetailDummy = {
+  "projectDetails": {
+    "name": "Aurora 2023",
+    "description": "Proyecto 2023 en USA",
+    "latitude": 44.97997,
+    "longitude": -93.26384,
+    "plantType": "Fotovoltaica",
+    "trackerType": "SF7",
+    "power": "150 MW",
+    "electronicType": "Fotovoltaica"
+  },
+  "instances": [
+    {
+      "id": "ins1",
+      "name": "Instancia 001",
+      "description": "primera versión",
+      "creator": "Diego Pérez",
+      "createdDate": "2023-10-25",
+      "updatedDate": "2023-10-26",
+      "status": "Terminado"
+    },
+    {
+      "id": "ins2",
+      "name": "Instancia 002",
+      "description": "primera versión",
+      "creator": "Anastasia G",
+      "createdDate": "2023-06-15",
+      "updatedDate": "2023-06-26",
+      "status": "Terminado"
+    },
+    {
+      "id": "ins3",
+      "name": "Instancia 003",
+      "description": "primera versión",
+      "creator": "Cristina",
+      "createdDate": "2023-04-12",
+      "updatedDate": "2023-04-16",
+      "status": "En proceso"
+    },
+    {
+      "id": "ins4",
+      "name": "Instancia 004",
+      "description": "primera versión",
+      "creator": "Roslyn A",
+      "createdDate": "2023-07-22",
+      "updatedDate": "2023-07-23",
+      "status": "En proceso"
+    },
+    {
+      "id": "ins5",
+      "name": "Instancia 005",
+      "description": "primera versión",
+      "creator": "Gabriella Schlze",
+      "createdDate": "2022-11-25",
+      "updatedDate": "2022-11-26",
+      "status": "Terminado"
+    },
+    {
+      "id": "ins6",
+      "name": "Instancia 006",
+      "description": "primera versión",
+      "creator": "Alejandra Artiga",
+      "createdDate": "2022-11-29",
+      "updatedDate": "2022-10-30",
+      "status": "Terminado"
+    },
+    {
+      "id": "ins7",
+      "name": "Instancia 007",
+      "description": "primera versión",
+      "creator": "Lucas Vieria",
+      "createdDate": "2023-12-15",
+      "updatedDate": "2023-10-16",
+      "status": "Terminado"
+    },
+    {
+      "id": "ins8",
+      "name": "Instancia 008",
+      "description": "primera versión",
+      "creator": "Paola Quceno",
+      "createdDate": "2024-01-23",
+      "updatedDate": "2024-01-24",
+      "status": "En proceso"
+    },
+    {
+      "id": "ins9",
+      "name": "Instancia 009",
+      "description": "primera versión",
+      "creator": "Andrew Tubi",
+      "createdDate": "2024-01-11",
+      "updatedDate": "2024-01-14",
+      "status": "Terminado"
+    },
+  ],
+  "history": [
+    {
+      "user": "Diego Pérez",
+      "changes": "Cambios en Instancia 004",
+      "date": "2023-10-26T13:45:00+00:00"
+    },
+    {
+      "user": "Diego Pérez",
+      "changes": "Cambios en Instancia 004",
+      "date": "2023-10-26T13:45:00+00:00"
+    },
+    {
+      "user": "Diego Pérez",
+      "changes": "Cambios en Instancia 004",
+      "date": "2023-10-26T13:45:00+00:00"
+    },
+    {
+      "user": "Diego Pérez",
+      "changes": "Cambios en Instancia 004",
+      "date": "2023-10-26T13:45:00+00:00"
+    },
+    {
+      "user": "Diego Pérez",
+      "changes": "Cambios en Instancia 004",
+      "date": "2023-10-26T13:45:00+00:00"
+    },
+    {
+      "user": "Diego Pérez",
+      "changes": "Cambios en Instancia 004",
+      "date": "2023-10-26T13:45:00+00:00"
+    },
+    {
+      "user": "Diego Pérez",
+      "changes": "Cambios en Instancia 004",
+      "date": "2023-10-26T13:45:00+00:00"
+    }
+  ]
+};
+
+export const projectsDetailDummy_1 = {
+  "projectDetails": {
+    "name": "Aurora 2023",
+    "description": "Proyecto 2023 en USA",
+    "latitude": 44.97997,
+    "longitude": -93.26384,
+    "plantType": "Fotovoltaica",
+    "trackerType": "SF7",
+    "power": "150 MW",
+    "electronicType": "Fotovoltaica"
+  },
+  "instances": [],
+  "history": []
+};
