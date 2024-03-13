1d8f23a2184c4e342c115ebdb3e781517dd580a5
ccryptoDev <92884804+ccryptoDev@users.noreply.github.com>
2024-03-13 01:50:07 -0400

    added some dummy data

---
+++ src/app/utils/project-members.dummy.ts
@@ -0,0 +1,20 @@
+export const projectMembersDummy = [
+  {
+    profileImg: '',
+    firstName: 'Diego',
+    lastName: 'Perez',
+    permission: 'Admin'
+  },
+  {
+    profileImg: '',
+    firstName: 'Alvaro',
+    lastName: 'Moreno',
+    permission: 'Editor'
+  },
+  {
+    profileImg: '',
+    firstName: 'Patricia',
+    lastName: 'Rubio',
+    permission: 'Visor'
+  },
+]
---
+++ src/app/utils/projects.dummy.ts
@@ -0,0 +1,56 @@
+export const projectsDummy = [
+  {
+    "id": 1,
+    "name": "Atacama",
+    "description": "Descripción del proyecto",
+    "creator": "Vera",
+    "createdDate": "2023-10-25",
+    "updatedDate": "2023-10-25",
+    "permission": "Admin"
+  },
+  {
+    "id": 2,
+    "name": "Castile-La Mancha",
+    "description": "Descripción del proyecto",
+    "creator": "Mario",
+    "createdDate": "2023-09-25",
+    "updatedDate": "2023-10-25",
+    "permission": "Admin"
+  },
+  {
+    "id": 3,
+    "name": "Atacama",
+    "description": "Descripción del proyecto",
+    "creator": "Hernadez",
+    "createdDate": "2023-02-25",
+    "updatedDate": "2023-04-25",
+    "permission": "Visor"
+  },
+  {
+    "id": 4,
+    "name": "Proyecto004",
+    "description": "Descripción del proyecto",
+    "creator": "Gabriela",
+    "createdDate": "2024-01-02",
+    "updatedDate": "2024-01-25",
+    "permission": "Admin"
+  },
+  {
+    "id": 5,
+    "name": "Proyecto005",
+    "description": "Descripción del proyecto",
+    "creator": "Sebastian",
+    "createdDate": "2022-10-25",
+    "updatedDate": "2022-11-25",
+    "permission": "Sin permiso"
+  },
+  {
+    "id": 6,
+    "name": "Proyecto003",
+    "description": "Descripción del proyecto",
+    "creator": "Roberto",
+    "createdDate": "2022-12-22",
+    "updatedDate": "2023-12-25",
+    "permission": "Editor"
+  }
+];
