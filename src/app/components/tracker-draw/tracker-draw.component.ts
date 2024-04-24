bf189bf29adf1ee25696a33dd96e37b25da7fd87
Success126 <golbensta@gmail.com>
2024-04-24 15:24:20 -0400

    changed same as figma

--- src/app/components/tracker-draw/tracker-draw.component.ts
+++ src/app/components/tracker-draw/tracker-draw.component.ts
@@ -10,12 +10,12 @@ export class TrackerDrawComponent {
 
   ngAfterViewInit(): void {
     const ctx = this.canvas.nativeElement.getContext('2d');
-    ctx.beginPath();
-    ctx.moveTo(50, 50);   // Move to the first point
-    ctx.lineTo(200, 50);  // Draw a line to the second point
-    ctx.lineTo(200, 150); // Draw a line to the third point
-    ctx.lineTo(50, 150);  // Draw a line to the fourth point
-    ctx.closePath();      // Close the path to complete the rectangle
-    ctx.stroke();         // Stroke the rectangle
+    // ctx.beginPath();
+    // ctx.moveTo(50, 50);   // Move to the first point
+    // ctx.lineTo(200, 50);  // Draw a line to the second point
+    // ctx.lineTo(200, 150); // Draw a line to the third point
+    // ctx.lineTo(50, 150);  // Draw a line to the fourth point
+    // ctx.closePath();      // Close the path to complete the rectangle
+    // ctx.stroke();         // Stroke the rectangle
   }
 }
