f763537189a96c640a609dc59ed1722d4b96cde5
Success126 <golbensta@gmail.com>
2024-04-25 06:25:02 -0400

    step 2_2 map implement

---
+++ src/app/components/drag-drop-list/drag-drop-list.component.ts
@@ -0,0 +1,54 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {NgFor} from '@angular/common';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+} from '@angular/cdk/drag-drop';
+import { InstanceTrackerDrawList, TrackerInformation } from 'src/app/models/instance.model';
+
+@Component({
+  selector: 'app-drag-drop-list',
+  templateUrl: './drag-drop-list.component.html',
+  styleUrls: ['./drag-drop-list.component.scss'],
+  standalone: true,
+  imports:[CdkDropList, NgFor, CdkDrag]
+})
+export class DragDropListComponent implements OnInit {
+  @Input() data?: InstanceTrackerDrawList;
+  
+  @Output() onDrop = new EventEmitter<any>()
+
+  todo: TrackerInformation[] = [];
+
+  done: TrackerInformation[] = [];
+
+  ngOnInit(): void {
+    this.done = this.data?.possible_trackers ?? [];
+    this.todo = this.data?.trackers ?? []
+  }  
+
+  didDrop(event: CdkDragDrop<any[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      this.onDrop.emit({
+        todo: this.todo, 
+        done: this.done
+      })
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+
+      this.onDrop.emit({
+        todo: this.todo, 
+        done: this.done
+      })
+    }
+  }
+}
--- src/app/components/tracker-draw/tracker-draw.component.ts
+++ src/app/components/tracker-draw/tracker-draw.component.ts
@@ -1,23 +1,153 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { fabric } from 'fabric';
+import { InstanceComponent } from '../instance/instance.component';
+import { InstanceTracker, InstanceTrackerDrawList, TrackerInformation } from '../../models/instance.model';
+import { instanceTrackerDrawDummy } from 'src/app/utils/instance.dummy';
+
 
 @Component({
   selector: 'app-tracker-draw',
   templateUrl: './tracker-draw.component.html',
   styleUrls: ['./tracker-draw.component.scss']
 })
-export class TrackerDrawComponent implements AfterViewInit {
-  @ViewChild('myCanvas', { static: true }) canvas!: ElementRef<HTMLCanvasElement>;
+export class TrackerDrawComponent implements AfterViewInit, OnChanges {
+  @Input() data?: TrackerInformation[] = []
+  @Input() tc1?: number = 0
+  @Input() tc2?: number = 0
+  @Input() tc3?: number = 0
+  
+
+  @ViewChild('canvas', { static: true }) canvas!: ElementRef<HTMLCanvasElement>;
+  private ctx!: CanvasRenderingContext2D;
+  private isPanning: boolean = false;
+  private lastX: number = 0;
+  private lastY: number = 0;
+  private scale: number = 1;
+  private translateX: number = 0;
+  private translateY: number = 0;
 
   ngAfterViewInit(): void {
-    const ctx = this.canvas.nativeElement.getContext('2d');
-    if (ctx) {
-      // Draw a red rectangle
-      ctx.fillStyle = 'red';
-      ctx.fillRect(50, 50, 100, 75);
 
-      // Draw a blue rectangle
-      ctx.fillStyle = 'blue';
-      ctx.fillRect(200, 100, 150, 100);
+    const canvasElement = this.canvas.nativeElement;
+    this.ctx = canvasElement.getContext('2d')!;
+    // Draw initial rectangles
+    this.drawRectangles();
+
+    // Add event listeners for mouse interaction
+    this.canvas.nativeElement.addEventListener('mousedown', this.onMouseDown.bind(this));
+    this.canvas.nativeElement.addEventListener('mousemove', this.onMouseMove.bind(this));
+    this.canvas.nativeElement.addEventListener('mouseup', this.onMouseUp.bind(this));
+  }
+
+  updateCoordinateSystem() {
+    this.ctx.setTransform(this.scale, 0, 0, this.scale, this.translateX, this.translateY);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // this.drawRectangles();
+    this.data = changes['data']['currentValue']
+    const canvasElement = this.canvas.nativeElement;
+    this.ctx = canvasElement.getContext('2d')!;
+    this.drawRectangles();
+  }
+
+  drawRectangles() {
+
+    this.ctx.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);  // Clear the canvas
+    this.ctx.scale(this.scale, this.scale)
+    this.updateCoordinateSystem();  // Update the coordinate system
+
+    // Draw rectangles in the new coordinate system
+    for (let itemRect in this.data) {
+      this.consoleLog(this.data[(parseInt(itemRect))])
+      this.ctx.fillStyle = 'black';
+      this.ctx.beginPath();
+      this.ctx.moveTo(this.data[(parseInt(itemRect))]['point_SW'][0], this.data[(parseInt(itemRect))]['point_SW'][1]);
+      this.ctx.lineTo(this.data[(parseInt(itemRect))]['point_SE'][0], this.data[(parseInt(itemRect))]['point_SE'][1]);
+      this.ctx.lineTo(this.data[(parseInt(itemRect))]['point_NW'][0], this.data[(parseInt(itemRect))]['point_NW'][1]);
+      this.ctx.lineTo(this.data[(parseInt(itemRect))]['point_NE'][0], this.data[(parseInt(itemRect))]['point_NE'][1]);
+      this.ctx.closePath();
+      this.ctx.fill();
+      this.ctx.strokeStyle = 'white';
+      this.ctx.stroke();
     }
+    
+  }
+
+  onMouseDown(event: MouseEvent) {
+    this.isPanning = true;
+    [this.lastX, this.lastY] = [event.offsetX, event.offsetY];
+  }
+
+  onMouseMove(event: MouseEvent) {
+    if (this.isPanning) {
+      const deltaX = event.offsetX - this.lastX;
+      const deltaY = event.offsetY - this.lastY;
+      this.translateX += deltaX;
+      this.translateY += deltaY;
+      this.drawRectangles();
+      [this.lastX, this.lastY] = [event.offsetX, event.offsetY];
+    }
+  }
+
+  onMouseUp() {
+    this.isPanning = false;
+  }
+  private fabricCanvas!: fabric.Canvas;
+  // private isPanning = false;
+  // private lastPosX = 0;
+  // private lastPosY = 0;
+
+  // ngAfterViewInit(): void {
+  //   this.fabricCanvas = new fabric.Canvas(this.canvas.nativeElement, {
+  //     backgroundColor: '#f0f0f0', // Set your desired background color
+  //     selection: false, // Disable object selection
+  //     width: 800,
+  //     height: 600
+  //   });
+
+  //   this.fabricCanvas.on('mouse:down', (event) => {
+  //     this.isPanning = true;
+  //     const pointer = this.fabricCanvas.getPointer(event.e);
+  //     this.lastPosX = pointer.x;
+  //     this.lastPosY = pointer.y;
+  //   });
+
+  //   this.fabricCanvas.on('mouse:move', (event) => {
+  //     if (this.isPanning && event && event.e) {
+  //       const pointer = this.fabricCanvas.getPointer(event.e);
+  //       const delta = new fabric.Point(pointer.x - this.lastPosX, pointer.y - this.lastPosY);
+  //       this.fabricCanvas.relativePan(delta);
+  //       this.lastPosX = pointer.x;
+  //       this.lastPosY = pointer.y;
+  //     }
+  //   });
+
+  //   this.fabricCanvas.on('mouse:up', () => {
+  //     this.isPanning = false;
+  //   });
+
+  //   // Add objects to the canvas
+  //   const rect = new fabric.Rect({
+  //     left: 100,
+  //     top: 100,
+  //     width: 200,
+  //     height: 150,
+  //     fill: 'red'
+  //   });
+  //   this.fabricCanvas.add(rect);
+
+  zoomIn() {
+    this.scale *= 1.1;  // Increase the scale by 10%
+    this.drawRectangles();  // Redraw the content with the new scale
+  }
+
+  zoomOut() {
+    this.scale /= 1.1;  // Decrease the scale by 10%
+    this.drawRectangles();  // Redraw the content with the new scale
+  }
+
+  consoleLog(data: any) {
+    console.log("udpated data", data)
   }
 }
